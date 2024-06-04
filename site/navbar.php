cc10ab09bcb8e6dae84be869292e5edb32037634
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-06-04 01:04:26 -0300

    Update navbar.php
    tirei a imagen de 'user' e deixei somente o botão "login"

--- site/navbar.php
+++ site/navbar.php
@@ -1,21 +1,20 @@
-   <!--navbar start--> <!--INÍCIO-->
-   <header>
+<header>
        <nav>
-           <a class="logo" href="../site/index.php">NEWS WALL ETEC</a>
-           <div class="mobile-menu">
-               <div class="line1"></div>
-               <div class="line2"></div>
-               <div class="line3"></div>
-           </div>
+        <a class="logo">NEWS WALL ETEC</a>
+        <div class="mobile-menu">
+            <div class="line1" href="">Início</div>
+            <div class="line2" href="">Sobre</div>
+            <div class="line3" href="">Desenvolvedores</div>
+            <div class="line4" href="./contato.php">Contato</div>
+            <div class="line5" href="./login.php">Login</div>
+        </div> 
            <ul class="nav-list">
                <li><a href="./index.php">Início</a></li>
                <li><a href="#sobre">Sobre</a></li>
                <li><a href="#cards">Desenvolvedores</a></li>
                <li><a href="./contato.php">Contato</a></li>
-           </ul>
-           <div class="nav-img">
-               <a href="./login.php"><img src="https://cdn3.iconfinder.com/data/icons/cyber-monday-104/32/user_profile_people_member_avatar_account-1024.png"></a>
-           </div>
+                <a class="nav-button" href="./login.php"><button>Login</button></a>
+           </ul>         
        </nav>
    </header>
    <!--navbar end-->
