a9281a94a1cd4572739007e1117fd1d4a892a64b
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-06-04 01:09:04 -0300

    Update login.php
      a tela de login agora não mostra a navbar nem o footer, mais pra frente(quando a gente aprender a fazer) vai ser obrigatório criar uma conta para acessar o site, seja de aluno, professor, responsável ou admin.
      eu também linkei com um java.script para deixar responsivo

--- site/login.php
+++ site/login.php
@@ -5,47 +5,60 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LoginScreen</title>
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/estilo.css">
 </head>
 
 <body>
 
-  <?php include './navbar.php' ?>
+
     <!--login start--> <!--INÍCIO-->
-    <section class="container-login">
+    <section class="container-login-principal">
 
-        <div class="login">
-            <div class="card-login">
-                <form class="faca-login" name="login" action="#" method="post">
-                    <h1>LOGIN</h1>
-                    <p>Novo por aqui? <a href="#">Crie uma conta</a></p>
+        <div class="login-principal">
 
-                    <div class="textfield">
-                        <label for="usuario">Usuário</label>
-                        <input type="text" name="usuario" placeholder="Usuário">
-                    </div>
-
-                    <div class="textfield">
-                        <label for="email">E-mail</label>
-                        <input type="email" name="email" placeholder="E-mail">
-                    </div>
-
-                    <div class="textfield">
-                        <label for="senha">Senha</label>
-                        <input type="password" name="senha" placeholder="Senha">
-                    </div>
-
-
-                    <div class="enviar">
-                        <input type="submit" value="Entrar">
-                    </div>
-                </form>
-            </div>
+        <div class="conteiner-anun-login">
+           <a href="./index.php">NEWS WALL ETEC</a>
+           <h3>Faça login para ter acesso aos mais diversos tipos de funcionalidades e ferramentas de nosso site e se manter ligado em tudo que ocorre na ETEC</h3>
         </div>
 
+            <div class="container-login">
+                <div class="login">
+                    <div class="card-login">
+                        <form class="faca-login" name="login" action="#" method="post">
+                            <h1>LOGIN</h1>
+                            <p>Novo por aqui? <a href="#">Crie uma conta</a></p>
+
+                            <div class="textfield">
+                                <label for="usuario">Usuário</label>
+                                <input type="text" name="usuario" placeholder="Usuário">
+                            </div>
+
+                            <div class="textfield">
+                                <label for="email">E-mail</label>
+                                <input type="email" name="email" placeholder="E-mail">
+                            </div>
+
+                            <div class="textfield">
+                                <label for="senha">Senha</label>
+                                <input type="password" name="senha" placeholder="Senha">
+                            </div>
+
+
+                            <div class="enviar">
+                                <input type="submit" value="Entrar">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
 
-    <?php include './footer.php' ?>
 
+
+    <script src="./js/java.js"></script>
+</body>
+
+</html>
 </body>
 </html>
