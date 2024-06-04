153a6f7935f69716f9bf686f4537424b9c00eb34
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-06-04 01:12:46 -0300

    Update footer.php
    Mudei as imagens das redes sociais(agora elas não são puxadas por links externos  já que estão salvas no github) e deixei responsivo(de maneira bem porca mas deixei)

--- site/footer.php
+++ site/footer.php
@@ -1,54 +1,66 @@
-
-    <!--footer start-->  <!--CONTATO--> 
-    <footer class="footer">
+<!--footer start--> <!--CONTATO-->
+<footer class="footer">
+    <div class="footer-01-hr">
         <div class="footer-01">
             <div class="contact">
-                <h1>Inscreva-se</h1>
+                <div class="inscreva-se">
+                    <h1>Inscreva-se</h1>
+                </div>
                 <div class="btn-footer-button">
-                    <a href="#"><button>Acesse o site =></button></a>
+                    <a href="#"><button>Contate-nos </button></a>
                 </div>
             </div>
         </div>
-        <hr>
+    </div>
+
+    <div class="footer-02-hr">
         <div class="footer-02">
 
             <div class="footer-part">
                 <div class="left-logo">
                     <h3>NewsEtec®</h3>
+                    <hr>
                 </div>
-                <p>inspirado em Designs encotrados no dorik.com/blog/website-footer-examples</p>
+                <h2>inspirado em Designs encotrados no dorik.com/blog/website-footer-examples</h2>
             </div>
-            <hr>
+
             <div class="footer-part">
                 <h1>Criadores</h1>
+                <hr>
                 <H2>Guilherme Fermino de Souza</H2>
                 <H2>Gustavo Fermino de Souza</H2>
                 <H2>Tiago Soares Magalhães</H2>
                 <H2>Leonardo Lima Souza lopes</H2>
             </div>
-            <hr>
+
             <div class="footer-part">
                 <h1>Quer estar por dentro de tudo que ocorre na Etec?</h1>
+                <hr>
                 <H2>Inscreva-se para uma melhor experiência =></H2>
                 <H2>Siga-nos</H2>
                 <div class="right-icons">
-                    <a href="#"><img src="https://yandex-images.clstorage.net/ZjhW49251/2905aejXyLBh/CdhHiohCZFzQOclbgtIy3FtoYjODR40-z699UmEvn64GtUB1-z1taRG9peljuHMSh25eqj0mkEqXvpn8E3ixQQ3sg7651-FYcV0Nm_JCcyjqvOI38oKtj74fuMJUPAlcBMJTl21f30uW2RCptthCg2LOy0yuh-MMJz4-HaOKA7FpuXYLf6DWD7dJSWnwovLaG2pSZ5EirYwont83g37oXjSzYIT9XC6ZlpohqbS7AYTDrqrPrcJirOUvPowB2YKQWHq0S5_zhO_EO8jIM7KBukrbMmUTwvzMmj-vdGOOelxXQ_ClTe_MKXZ7oynFuzOlE7worW_hgjwFvGwtolohwgr4R7tMslRNJigaO6MCsHqbOEAnYTAvDtsc_HYjjlhMJOIhdM1PvZoWGMKLpduQpOHsev89k3C9NO88DzMJgWI4incqPLKFXPV7O5kTgqLYyglS9NDiL14o_z63cT2IXlSiYxbN7V5btnhwiYe6Q0YTnXuv_jHyXgbeX54De7IDa7q3Of4h1G906zh54LHwOvtIAHcQkd_-CG0dV7FsWJ41sgM0jT--maW6MZvV2JFlQt6ZjP2hIk6kzi99ICjjw3rZFaq8YpU9JPrLW8DDcgjrChPkYJCv77qvvBThbNr9tbBSx44e7DilqGIbdjhwJsAu2M88MSI95J-PjbHI8nKbmOT4H5Amnbdb-uvhsLDIm1ixldHgjU4ZXy1H47_YbOXQUvafHV_LZOuAaBTKc6fgTQkNPzPRPtWuzF2hGnHTe4t3290gBE8kGigKYYAj64vZMgXiwl_c6XwuVYNfuJyUouEFH_38aXWpwFjHC5BX88563h-hEG6Xzg4vUDmQYFs5F4osUxTN9fr6exHQsZua6qJm8sLe_Djfb8fjH0hPZ-ESdxwvPlm2-pPoxLmSJKIMKb0OEfFs1e2enwJ74ZNL61cK7HOFXHeaqmjgMiB7-sgCN_Oi_vy43j1FI" alt="#"></a>
-                    <a href="#"><img src="https://www.pngitem.com/pimgs/m/105-1054537_linkedin-icon-free-png-and-svg-download-cnn.png" alt="#"></a>
-                    <a href="#"><img src="https://pagedairymart.net/wp-content/uploads/2020/03/instagram-png-instagram-png-logo-1455-copy-1030x1030.png" alt="#"></a>
-                    <a href="#"><img src="https://ico.cppng.com/download/2114/phone_PNG48961.png" alt="#"></a>
+                    <a href="#"><img src="./images/Icon-facebook.png" alt="#"></a>
+                    <a href="#"><img src="./images/Icon-linkedin.png" alt="#"></a>
+                    <a href="#"><img src="./images/Icon-instagram.png" alt="#"></a>
+                    <a href="#"><img src="./images/Icon-whatssap.png" alt="#"></a>
                 </div>
             </div>
-            <hr>
+
         </div>
+    </div>
+
+    <div class="footer-bottom-hr">
         <hr>
         <div class="footer-bottom">
+
             <h1>NewsEtec 2024</h1>
             <ul class="nav-list">
-                <li><a href="#conteudo">Início</a></li>
-                <li><a href="#sobre">Sobre</a></li>
-                <li><a href="#cards">Desenvolvedores</a></li>
-                <li><a href="#">Contato</a></li>
+                <li><a href="#">Início</a></li>
+                <li><a href="#">Sobre</a></li>
+                <li><a href="#">Desenvolvedores</a></li>
+                <li><a href="./login.php">Login</a></li>
             </ul>
         </div>
-    </footer>
-    <!--footer end-->
+    </div>
+</footer>
+<!--footer end-->
