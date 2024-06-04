56d813e97ea4a4e5456c54ea1bdea7cb2b3011f4
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-06-04 01:14:37 -0300

    Update contato.php
    mudei o estilo, depois olha pra ver se o mySQL ainda tá pegando

--- site/contato.php
+++ site/contato.php
@@ -17,57 +17,63 @@
         <div class="contato">
 
             <form class="fale-conosco" name="fale-conosco" action="contatoEnviar.php" method="post">
-                <div class="inputs">
-                    <h1 class="titulo-contato">Fale Conosco</h1>
-                    <div class="espaco-input">
-                        <label for="nome" class="label">Nome:</label>
-                        <input type="text" class="input" name="nome" placeholder="Seu Nome" maxlength="70" />
-                    </div>
 
-
-                    <div class="espaco-input">
-                        <label for="email" class="label">Email:</label>
-                        <input type="email" class="input" name="email" placeholder="E-mail" maxlength="70" />
-                    </div>
-
-                    <div class="espaco-input">
-                        <label for="sala" class="label">Sala:</label>
-                        <select class="input" name="sala">
-                            <option value="1">1° DS</option>
-                            <option value="2">1° Nutri A</option>
-                            <option value="3">1° Nutri B</option>
-                            <option value="4">2° DS</option>
-                            <option value="5">2° Nutri A</option>
-                            <option value="6">2° Nutri B</option>
-                            <option value="7">3° DS A</option>
-                            <option value="8">3° DS B</option>
-                            <option value="9">3° Nutri</option>
-                            <option value="10">Modular</option>
-                        </select>
-                    </div>
-                </div>
+                <h1 class="titulo-contato">Fale Conosco</h1>
 
                 <div class="inputs">
-                    <div class="espaco-input">
-                        <label for="rm" class="label">RM:</label>
-                        <input type="Numeric" class="input" name="rm" placeholder="Digite seu RM" />
+
+                    <div class="inputs-first-column">
+                        <div class="espaco-input">
+                            <label for="nome" class="label">Nome:</label>
+                            <input type="text" class="input" name="nome" placeholder="Seu Nome" />
+                        </div>
+
+
+                        <div class="espaco-input">
+                            <label for="email" class="label">Email:</label>
+                            <input type="email" class="input" name="email" placeholder="E-mail" />
+                        </div>
+
+                        <div class="espaco-input">
+                            <label for="sala" class="label">Sala:</label>
+                            <select class="input" name="sala">
+                                <option value="1° DS">1° DS</option>
+                                <option value="1° Nutri A">1° Nutri A</option>
+                                <option value="1° Nutri B">1° Nutri B</option>
+                                <option value="2° DS">2° DS</option>
+                                <option value="2° Nutri A">2° Nutri A</option>
+                                <option value="2° Nutri B">2° Nutri B</option>
+                                <option value="3° DS A">3° DS A</option>
+                                <option value="3° DS B">3° DS B</option>
+                                <option value="3° Nutri">3° Nutri</option>
+                                <option value="Modular">Modular</option>
+                            </select>
+                        </div>
                     </div>
 
 
-                    <div class="espaco-input">
-                        <label for="assunto" class="label">Assunto:</label>
-                        <input type="text" class="input" name="assunto" placeholder="Assunto da Mensagem" maxlength="100" />
-                    </div>
+                    <div class="inputs-second-column">
+                        <div class="espaco-input">
+                            <label for="rm" class="label">RM:</label>
+                            <input type="Numeric" class="input" name="rm" placeholder="Digite seu RM" />
+                        </div>
 
 
-                    <div class="espaco-input">
-                        <label for="mensagem" class="label">Mensagem:</label>
-                        <input type="text" class="input" name="mensagem" placeholder="Digite sua Mensagem Aqui" maxlength="1000" />
-                    </div> <!--Deixe este placeholder maior-->
+                        <div class="espaco-input">
+                            <label for="assunto" class="label">Assunto:</label>
+                            <input type="text" class="input" name="assunto" placeholder="Tema da mensagem" />
+                        </div>
 
 
-                    <div class="contato-enviar">
-                        <input type="submit" class="botao-enviar" name="botao" placeholder="Enviar" />
+                        <div class="espaco-input">
+                            <label for="mensagem" class="label">Mensagem:</label>
+                            <input type="text" class="input" name="mensagem" placeholder="Digite sua mensagem aqui" />
+                        </div> <!--Deixe este placeholder maior-->
+
+
+                        <div class="contato-enviar">
+                            <input type="submit" class="botao-enviar" name="botao" placeholder="Enviar" />
+                        </div>
                     </div>
                 </div>
             </form>
@@ -77,6 +83,7 @@
 
     <?php include './footer.php' ?>
 
+    <script src="./js/java.js"></script>
 </body>
 
 </html>
