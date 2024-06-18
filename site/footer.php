33d008dce3ea8b491fc0998767873abf326f7e4d
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-06-17 21:50:23 -0300

    Update footer.php
    o que eu mudei?
    
    
    
    
    não é da sua conta

--- site/footer.php
+++ site/footer.php
@@ -1,36 +1,29 @@
 <!--footer start--> <!--CONTATO-->
 <footer class="footer">
-    <div class="footer-01-hr">
-        <div class="footer-01">
-            <div class="inscreva-se">
-                <h1>Inscreva-se para os Vestibulhos da Etec</h1>
-            </div>
-            <a href="#" class="inscreva-se-button">Inscreva-se</a>
-        </div>
-    </div>
 
     <div class="footer-02-hr">
         <div class="footer-02">
 
             <div class="footer-part">
-                <h1>Criadores</h1>
-                <hr>
-                <div class="criadores-lista">
-                    <div>
-                        <h2>Guilherme Fermino de Souza</h2>
-                        <h2>Gustavo Fermino de Souza</h2>
-                    </div>
-                    <div>
-                        <h2>Tiago Soares Magalhães</h2>
-                        <h2>Leonardo Lima Souza lopes</h2>
-                    </div>
+                <div class="left-logo">
+                <img src="https://2.bp.blogspot.com/-YJcEKDgkCac/T3o4oD-0DrI/AAAAAAAAAQY/DFbIKXFJxbk/s1600/Logo+Etec+CT.png">
                 </div>
             </div>
 
             <div class="footer-part">
-                <h1>Siga-nos</h1>
+                <h1>Criadores</h1>
                 <hr>
-                </hr>
+                <H2>Guilherme Fermino de Souza</H2>
+                <H2>Gustavo Fermino de Souza</H2>
+                <H2>Tiago Soares Magalhães</H2>
+                <H2>Leonardo Lima Souza lopes</H2>
+            </div>
+
+            <div class="footer-part">
+                <h1>Quer estar por dentro de tudo que ocorre na Etec?</h1>
+                <hr>
+                <H2>Inscreva-se para uma melhor experiência =></H2>
+                <H2>Siga-nos</H2>
                 <div class="right-icons">
                     <a href="#"><img src="./images/Icon-facebook.png" alt="#"></a>
                     <a href="#"><img src="./images/Icon-linkedin.png" alt="#"></a>
@@ -38,16 +31,17 @@
                     <a href="#"><img src="./images/Icon-whatssap.png" alt="#"></a>
                 </div>
             </div>
-            <div class="footer-part">
-                <h1>Navegação</h1>
-                <hr>
-                <ul class="nav-list">
-                    <li><a href="#"><h2>Início</h2></a></li>
-                    <li><a href="#"><h2>Sobre</h2></a></li>
-                    <li><a href="#"><h2>Desenvolvedores</h2></a></li>
-                    <li><a href="./login.php"><h2>Login</h2></a></li>
-                </ul>
-            </div>
+
+        </div>
+    </div>
+
+    <div class="footer-bottom-hr">
+        <hr>
+        <div class="footer-bottom">
+            <h1 class="footer-bottom-h2">Website by Amiguinhos</h1>
+            <h1 class="footer-bottom-h1">
+                @Otidípote Farm 2007-2024
+            </h1>
         </div>
     </div>
 </footer>
