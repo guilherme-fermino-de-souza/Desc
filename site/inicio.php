95856eab148a0184d86f60d71e4f3982c13a7e8c
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-09-15 09:46:20 -0300

    Create inicio.php

---
+++ site/inicio.php
@@ -0,0 +1,9 @@
+<div class="container-inicio">
+    <div class="conteudo-inicio">
+        <div class="text-inicio">
+            <h1>NEWS WALL ETEC</h1>
+            <p>A Rede de Notícias da Etec de Guainases</p>
+            <a class="botao-inicio" href="./login.php"><button>Login</button></a>
+        </div>
+    </div>
+</div>
