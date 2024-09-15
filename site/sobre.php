7b0e3e7932257c9a02dd5b961d7d4ce5914abff2
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-09-15 15:49:56 -0300

    Update sobre.php

--- site/sobre.php
+++ site/sobre.php
@@ -1,17 +1,28 @@
 <div class="sobre">
     <div class="container-sobre">
+        <h1>NOTÍCIAS E EVENTOS DA ETEC</h1>
+        <h2>Rua Feliciano de Mendonça, 290 - Guaianazes - CEP: 08460-365 - SP</h2>
         <div class="text-sobre">
-            <h1>NOTÍCIAS E EVENTOS DA ETEC</h1>
-            <p> O News Wall Etec é um site/calendario que visa
-                facilitar o acesso a informações sobre eventos
-                de relevância na Etec, podendo ser usando por
-                professores, alunos, cordenadores e pelo público
-                em geral, com cada um tendo acesso a diferentes
-                informações e funcionalidades.</p>
+            <div class="img-sobre">
+                <img src="./images/vetores/News-pana.svg">
+            </div>  
+            <div class="paragrafo-sobre">
+                <p> O News Wall Etec oferece uma plataforma centralizada onde a comunidade acadêmica e o público
+                    externo podem se manter atualizados sobre os eventos e atividades mais importantes da instituição.
+                    Com uma interface intuitiva e acessível, o site é projetado para atender às necessidades variadas
+                     dos seus usuários, garantindo que todos possam encontrar as informações que precisam de maneira
+                     rápida e eficiente. Além de listar eventos, o site permite a integração com calendários pessoais
+                     e envia notificações sobre atualizações importantes, assegurando que ninguém perca um evento
+                     relevante.</p>
+                <br>
+                <p> Para os professores, o News Wall Etec não só proporciona uma visão clara dos eventos acadêmicos,
+                     como também permite o agendamento e a gestão de suas próprias atividades e compromissos. Os
+                     professores podem criar e atualizar eventos relacionados às suas disciplinas, como palestras,
+                     reuniões e exames, facilitando a organização e a comunicação com os alunos. Essa funcionalidade
+                     ajuda a manter a agenda acadêmica sincronizada e evita conflitos de horário, promovendo uma
+                     gestão mais eficiente do tempo e dos recursos.</p>
+            </div>
         </div>
-        <div class="img-sobre">
-            <img src="./images/Etec-Guaianases-1.jpg">
-            <h2>Rua Feliciano de Mendonça, 290 - Guaianazes - CEP: 08460-365 - SP</h2>
-        </div>
+      
     </div>
 </div>
