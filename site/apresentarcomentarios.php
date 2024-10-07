252c8e96bae39568b73b1e0cc3d15e5d2e5e72f5
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-10-07 15:34:00 -0300

    Update apresentarcomentarios.php

--- site/apresentarcomentarios.php
+++ site/apresentarcomentarios.php
@@ -5,15 +5,101 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../site/css/estilo.css">
     <style>
-        .conteiner-apresentarcomentario { /* PRINCIPAL */
-            display: grid;
+        /*COMENTÁRIO START Fazer comentário*/
+        .comentario-principal {
+        display: flex;
+        justify-content: center; /* Horizontal alignment */
+        align-items: center;     /* Vertical alignment */
+        background-color: var(--branco-principal);
+        background-position: center;
+        background-size: cover;
+        height: 85vh;
+        } 
+        .comentario {  /* Bloco de cometário */
+            display: flex;
+            flex-direction: column;
+            width: 25%;
+            background-color: var(--cinza-fonte-claro);
+            border: 2px solid var(--tema-secundario); /* Largura, estilo e cor da borda */
+            text-indent: 5%;
+            padding: 0.2% 0%;
+        }
+        .comente {
+            display: flex;
+            flex-direction: column;
             width: 80%;
             margin: auto;
-            grid-template-columns: 50%  50%;
+        }
+        .comentario h1 {
+            font-size: var(--fonte-grande);
+            margin: 0% 0% 2% 0%;
+            color: var(--preto-fonte);
+            font-weight: 800;
+            text-align: center;
+        }
+        .textfield {
+            display: flex;
+            flex-direction: column;
+            padding: 1%;
+        }
+        .textfield label {
+            font-size: var(--fonte-padrao);
+            font-weight: 700;
+            margin-top: 2%;
+        }
+        .textfield input::placeholder {
+            font-size: var(--fonte-padrao);
+            color: var(--preto-fonte);
+            padding-left: 4%;
+        }
+        .textfield>input:focus {
+            outline: none;
+            color: var(--preto-fonte);
+            background-color: rgba(0, 0, 0, 0);
+            border-radius: 10%;
+            transition: 0.5s;
+        }
+        .textfield>input {
+            width: 100%;
+            margin-top: 2%;
+            border: none;
+            border-bottom: 2px solid var(--tema-secundario);
+            background-color: rgba(0, 0, 0, 0.0); /* Define a cor de fundo com transparência */
+            color: var(--branco-principal);
+            font-size: var(--fonte-padrao);
+            box-shadow: none;
+            outline: none;
+        }
+        .comente input[type="submit"] {
+            background-color: var(--tema-terciario);
+            color: var(--branco-principal);
+            font-size: var(--fonte-padrao);
+            border-radius: 5px;
+            padding: 1% 0%;
+            width: 100%;
+            margin: 6% 0% ;
+        }
+
+        input[type="submit"]:hover {
+            background-color: var(--branco-principal);
+            color: var(--tema-terciario);
+            cursor: pointer;
+            transition: 0.5s;
+        }
+        @media (max-width: 1050px) {
+            .comentario-principal {
+            height: 90vh;
+        }
+        }
+        /*COMENTÁRIO END*/
+        .conteiner-apresentarcomentario { /* PRINCIPAL Apresentar comentário*/
+            display: grid;
+            width: 80%;
+            margin: 2% auto;
+            grid-template-columns: 100%;
         }
         .card-apresentarcomentario {   /* CARD */
             background-color: var(--branco-principal);
-            margin: 5% ;
             border-radius: 5px;
             border: 5px solid var(--tema-terciario);
             height: fit-content;
@@ -23,7 +109,7 @@
             display: flex;
         }
         .titulo-card-comentario h1{
-            font-size: var(--fonte-media);
+            font-size: var(--fonte-padrao);
             color: var(--branco-principal);
             padding: 1%;
         }
@@ -40,12 +126,12 @@
         }
         .comentario { /* SUBTÍTULO */
             background-color: var(--tema-secundario);
+            width: 100%;
         }
         .comentario h2{
             font-weight: 800;
             font-size: var(--fonte-padrao);
             color: var(--branco-principal);
-            padding: 1%;
         }
         .alterar-card-comentario { /* ALTERAR */
             display: flex;
@@ -62,22 +148,46 @@
             color:  var(--branco-principal);
         }
     </style>
-    <title>Apresentar Comentários</title>
+    <title>Apresentar comentario</title>
 </head>
 <body>
 <?php include('navbar.php');?>
 
 <?php include("conexao.php");?> 
+    <!--Fazer comentário-->
+<div class="contato">
 
-<div class="conteiner-apresentarcomentarios">
+            <form class="comente" name="comente" action="./inserirComentario.php" method="post">
+                <h1>COMENTE</h1>
+                <?php $aviso_id = $_GET['id']; // Captura o ID da notícia?>
+                <input type="hidden" name="idAviso" value="<?php echo htmlspecialchars($aviso_id); ?>">
+
+                       <div class="textfield">
+                            <label for="nome" class="label">Nome:</label>
+                            <input type="text" class="input" name="nome" required placeholder="Insira o Nome" />
+                        </div>
+
+                        <div class="textfield">
+                            <label for="comentario" class="label">Comentario:</label>
+                            <input type="text" class="input" name="comentario" required placeholder="Insira o Comentário" />
+                        </div>
+
+                        <div class="comentario-enviar">
+                            <input type="submit" class="botao-enviar" name="botao" required placeholder="Enviar" />
+                        </div>
+            </form>
+        </div>  
+<!--Apresentar comentário-->
+<div class="conteiner-apresentarcomentario">
+
     <?php
-        $stmt = $pdo->prepare("select * from tbcomentarionoticia");
-        $stmt -> execute();
+        $stmt = $pdo->prepare("SELECT * FROM tbComentarioNoticia WHERE aviso_id = ?");
+        $idAviso = $_GET['id'];
+        $stmt -> execute([$idAviso]);
         while($row = $stmt->fetch(PDO::FETCH_BOTH)){?>
             <div class="card-apresentarcomentario">
                 <div class="titulo-card-comentario">
-                    <h1>Notícia: <?php echo $row["aviso_id"]; ?>
-                        Nome: <?php echo $row["nomeComentarioNoticia"]; ?></h1>
+                    <h1><?php echo $row["nomeComentarioNoticia"]; ?></h1>
                     <a href="excluircomentario.php?id=<?php echo $row[0]; ?>"> X </a> 
                 </div>
                 <div class="comentario">
@@ -88,7 +198,6 @@
                 </div>
             </div>
     <?php } ?>  
-    
 </div>  
 
 <?php include ('footer.php')?>
