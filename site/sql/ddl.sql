0ff7ccd29ae7eea03ba5894406e6bd553e76c2c0
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-10-22 11:13:42 -0300

    Update ddl.sql

--- site/sql/ddl.sql
+++ site/sql/ddl.sql
@@ -15,8 +15,8 @@ CREATE TABLE tbNoticias (
 CREATE TABLE tbConta (
     idConta INT PRIMARY KEY AUTO_INCREMENT
     ,nomeConta VARCHAR (60)
-    ,emailConta VARCHAR (50)
-    ,senhaConta VARCHAR (30)
+    ,emailConta VARCHAR (60)
+    ,senhaConta VARCHAR (255)
     ,tipoConta VARCHAR(4)
 );
 /* Tabela das mensagens recebidas pelo contato*/
