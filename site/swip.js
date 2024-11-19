00e992c363aa643e97ae14cf6d949978841e0dd2
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-11-19 01:13:51 -0300

    Update swip.js

--- site/swip.js
+++ site/swip.js
@@ -1,29 +1,26 @@
-var swiper = new swiper(".slideCursos", {
-    slidesPerView: 2,
-    spaceBetween: 25,
-    loop: true,
-    centerSlide: 'true',
-    fade: 'true',
-    grabCursor: 'true',
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      dynamicBullets: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    breakpoints:{
-      0: {
-        slidesPerView: 1,
+const swiper = new Swiper(".slideCursos", { //S maiúsculo!!!!
+      loop: true,
+      slidesPerView: 2,
+      spaceBetween: 30,
+      grabCursor: 'true',
+      pagination: {
+        el: "swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
       },
-      750: {
-        slidesPerView: 2,
+      navigation: {
+        nextEl: "swiper-button-next",
+        prevEl: "swiper-button-prev",
       },
-    },
-      // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-  });
+      breakpoints:{
+        0: {
+          slidesPerView: 1,
+        },
+        460: {
+          slidesPerView: 2,
+        },
+        800: {
+          slidesPerView: 2,
+        },
+      },
+    });
